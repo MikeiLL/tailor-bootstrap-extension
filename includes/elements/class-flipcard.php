caf474fa35dccab0409cac74cc6a34d5c532cd0c
Mike iLL Kilmer <mike@mzoo.org>
2018-04-19 01:26:30 -0500

    Replace content with card content.

--- includes/elements/class-flipcard.php
+++ includes/elements/class-flipcard.php
@@ -15,101 +15,134 @@ if ( class_exists( 'Tailor_Element' ) && ! class_exists( 'Tailor_Flipcard_Elemen
      */
     class Tailor_Flipcard_Element extends Tailor_Element {
 
-	    /**
-	     * Registers element settings, sections and controls.
-	     *
-	     * @access protected
-	     */
-	    protected function register_controls() {
+        /**
+         * Registers element settings, sections and controls.
+         *
+         * @since 1.0.0
+         * @access protected
+         */
+        protected function register_controls() {
 
-		    $this->add_section( 'general', array(
-			    'title'                 =>  __( 'General' ),
-			    'priority'              =>  10,
-		    ) );
+            $this->add_section( 'general', array(
+                'title'                 =>  __( 'General', 'tailor' ),
+                'priority'              =>  10,
+            ) );
 
-		    $this->add_section( 'colors', array(
-			    'title'                 =>  __( 'Colors' ),
-			    'priority'              =>  30,
-		    ) );
+            $this->add_section( 'colors', array(
+                'title'                 =>  __( 'Colors', 'tailor' ),
+                'priority'              =>  20,
+            ) );
 
-		    $this->add_section( 'attributes', array(
-			    'title'                 =>  __( 'Attributes' ),
-			    'priority'              =>  40,
-		    ) );
+            $this->add_section( 'attributes', array(
+                'title'                 =>  __( 'Attributes', 'tailor' ),
+                'priority'              =>  30,
+            ) );
 
-		    $priority = 0;
+            $priority = 0;
 
-		    // Add as many custom settings as you like..
-		    $this->add_setting( 'title', array(
-			    'sanitize_callback'     =>  'tailor_sanitize_text',
-			    'default'               =>  'Default title',
-		    ) );
-		    $this->add_control( 'title', array(
-			    'label'                 =>  __( 'Title' ),
-			    'type'                  =>  'text',
-			    'section'               =>  'general',
-			    'priority'              =>  $priority += 10,
-		    ) );
+            $general_control_types = array(
+                'title',
+                'horizontal_alignment',
+                'horizontal_alignment_tablet',
+                'horizontal_alignment_mobile',
+                'image',
+            );
+            $general_control_arguments = array(
+                'title'                 =>  array(
+                    'setting'               =>  array(
+                        'default'               =>  $this->label,
+                    ),
+                ),
+            );
+            tailor_control_presets( $this, $general_control_types, $general_control_arguments, $priority );
 
-		    $this->add_setting( 'description', array(
-			    'sanitize_callback'     =>  'tailor_sanitize_text',
-		    ) );
-		    $this->add_control( 'description', array(
-			    'label'                 =>  __( 'Description' ),
-			    'type'                  =>  'text',
-			    'section'               =>  'general',
-			    'priority'              =>  $priority += 10,
-		    ) );
+            $priority = 0;
+            $color_control_types = array(
+                'color',
+                'link_color',
+                'link_color_hover',
+                'heading_color',
+                'background_color',
+                'border_color',
+            );
+            $color_control_arguments = array();
+            tailor_control_presets( $this, $color_control_types, $color_control_arguments, $priority );
 
-		    // See the custom content element for an example of how to use/modify standard control types
-		    $general_control_types = array();
-		    $general_control_arguments = array();
-		    tailor_control_presets( $this, $general_control_types, $general_control_arguments, $priority );
+            $priority = 0;
+            $attribute_control_types = array(
+                'class',
+                'padding',
+                'padding_tablet',
+                'padding_mobile',
+                'margin',
+                'margin_tablet',
+                'margin_mobile',
+                'border_style',
+                'border_width',
+                'border_width_tablet',
+                'border_width_mobile',
+                'border_radius',
+                'shadow',
+                'background_image',
+                'background_repeat',
+                'background_position',
+                'background_size',
+                'background_attachment',
+            );
+            $attribute_control_arguments = array();
+            tailor_control_presets( $this, $attribute_control_types, $attribute_control_arguments, $priority );
+        }
 
-		    // Standard color settings..
-		    $priority = 0;
-		    $color_control_types = array(
-			    'color',
-			    'link_color',
-			    'heading_color',
-			    'background_color',
-			    'border_color',
-		    );
-		    $color_control_arguments = array();
-		    tailor_control_presets( $this, $color_control_types, $color_control_arguments, $priority );
+        /**
+         * Returns custom CSS rules for the element.
+         *
+         * @since 1.0.0
+         *
+         * @param $atts
+         * @return array
+         */
+        public function generate_css( $atts = array() ) {
+            $css_rules = array();
+            $excluded_control_types = array(
+                'padding',
+                'padding_tablet',
+                'padding_mobile'
+            );
+            $css_rules = tailor_css_presets( $css_rules, $atts, $excluded_control_types );
 
-		    // Standard attribute settings..
-		    $priority = 0;
-		    $attribute_control_types = array(
-			    'class',
-			    'padding',
-			    'margin',
-			    'border_style',
-			    'border_width',
-			    'border_radius',
-			    'shadow',
-			    'background_image',
-			    'background_repeat',
-			    'background_position',
-			    'background_size',
-		    );
-		    $attribute_control_arguments = array();
-		    tailor_control_presets( $this, $attribute_control_types, $attribute_control_arguments, $priority );
-	    }
+            $selectors = array(
+                'padding'                   =>  array( '.tailor-card__content' ),
+                'padding_tablet'            =>  array( '.tailor-card__content' ),
+                'padding_mobile'            =>  array( '.tailor-card__content' ),
+            );
+            $css_rules = tailor_generate_attribute_css_rules( $css_rules, $atts, $selectors );
 
-	    /**
-	     * Returns custom CSS rules for the element.
-	     *
-	     * @param $atts
-	     * @return array
-	     */
-	    public function generate_css( $atts ) {
-		    $css_rules = array();
-		    $excluded_control_types = array();
+            // Header border color
+            if ( ! empty( $atts['border_color'] ) ) {
+                $css_rules[] = array(
+                    'setting'           =>  'border_color',
+                    'selectors'         =>  array( '.tailor-card__header' ),
+                    'declarations'      =>  array(
+                        'border-color'      =>  esc_attr( $atts['border_color'] ),
+                    ),
+                );
+            }
 
-		    // Just generate the default setting CSS
-		    $css_rules = tailor_css_presets( $css_rules, $atts, $excluded_control_types );
-		    return $css_rules;
-	    }
+            // Header image
+            if ( ! empty( $atts['image'] ) && is_numeric( $atts['image'] ) ) {
+                $background_image_info = wp_get_attachment_image_src( $atts['image'], 'full' );
+                $background_image_src = $background_image_info[0];
+                $css_rules[] = array(
+                    'setting'           =>  'image',
+                    'selectors'         =>  array( '.tailor-card__header' ),
+                    'declarations'      =>  array(
+                        'background'        =>  "url('{$background_image_src}') center center no-repeat",
+                        'background-size'   =>  'cover',
+                    ),
+                );
+            }
+
+            return $css_rules;
+        }
     }
 }
--- includes/shortcodes/shortcode-flipcard.php
+++ includes/shortcodes/shortcode-flipcard.php
@@ -12,57 +12,62 @@ if ( ! function_exists( 'tailor_shortcode_flipcard' ) ) {
      */
     function tailor_shortcode_flipcard_element( $atts, $content = null, $tag ) {
 
-	    /**
-	     * Filter the default shortcode attributes.
-	     *
-	     * @param array
-	     */
-	    $default_atts = apply_filters( 'tailor_shortcode_default_atts_' . $tag, array() );
-	    $atts = shortcode_atts( $default_atts, $atts, $tag );
-	    $html_atts = array(
-		    'id'            =>  empty( $atts['id'] ) ? null : $atts['id'],
-		    'class'         =>  explode( ' ', "tailor-element tailor-flip-card {$atts['class']}" ),
-		    'data'          =>  array(),
-	    );
+        /**
+         * Filter the default shortcode attributes.
+         *
+         * @since 1.6.6
+         *
+         * @param array
+         */
+        $default_atts = apply_filters( 'tailor_shortcode_default_atts_' . $tag, array() );
+        $atts = shortcode_atts( $default_atts, $atts, $tag );
+        $html_atts = array(
+            'id'            =>  empty( $atts['id'] ) ? null : $atts['id'],
+            'class'         =>  explode( ' ', "tailor-element tailor-card {$atts['class']}" ),
+            'data'          =>  array(),
+        );
 
-	    // Do something with the element settings
-	    $content = '';
-	    if ( ! empty( $atts['title'] ) ) {
-		    $content .= '<h2>' . esc_attr( $atts['title'] ) . '</h2>';
-	    }
-	    if ( ! empty( $atts['description'] ) ) {
-		    $content .= '<p>' . esc_attr( $atts['description'] ) . '</p>';
-	    }
+        if ( is_numeric( $atts['image'] ) ) {
+            $html_atts['class'][] = 'has-header-image';
+        }
 
-	    /**
-	     * Filter the HTML attributes for the element.
-	     *
-	     * @param array $html_attributes
-	     * @param array $atts
-	     * @param string $tag
-	     */
-	    $html_atts = apply_filters( 'tailor_shortcode_html_attributes', $html_atts, $atts, $tag );
-	    $html_atts['class'] = implode( ' ', (array) $html_atts['class'] );
-	    $html_atts = tailor_get_attributes( $html_atts );
+        /**
+         * Filter the HTML attributes for the element.
+         *
+         * @since 1.7.0
+         *
+         * @param array $html_attributes
+         * @param array $atts
+         * @param string $tag
+         */
+        $html_atts = apply_filters( 'tailor_shortcode_html_attributes', $html_atts, $atts, $tag );
+        $html_atts['class'] = implode( ' ', (array) $html_atts['class'] );
+        $html_atts = tailor_get_attributes( $html_atts );
 
-	    $outer_html = "<div {$html_atts}>%s</div>";
-	    $inner_html = '<div class="tailor-flip-card__content">%s</div>';
-	    $html = sprintf( $outer_html, sprintf( $inner_html, $content ) );
+        $title = ! empty( $atts['title'] ) ? '<h3 class="tailor-card__title">' . esc_html( (string) $atts['title'] ) . '</h3>' : '';
 
-	    /**
-	     * Filter the HTML for the element.
-	     *
-	     * @param string $html
-	     * @param string $outer_html
-	     * @param string $inner_html
-	     * @param string $html_atts
-	     * @param array $atts
-	     * @param string $content
-	     * @param string $tag
-	     */
-	    $html = apply_filters( 'tailor_shortcode_html', $html, $outer_html, $inner_html, $html_atts, $atts, $content, $tag );
+        $outer_html = "<div {$html_atts}>%s</div>";
+        $inner_html = '<header class="tailor-card__header">' . $title . '</header>' .
+            '<div class="tailor-card__content">%s</div>';
+        $content = do_shortcode( $content );
+        $html = sprintf( $outer_html, sprintf( $inner_html, $content ) );
 
-	    return $html;
+        /**
+         * Filter the HTML for the element.
+         *
+         * @since 1.7.0
+         *
+         * @param string $html
+         * @param string $outer_html
+         * @param string $inner_html
+         * @param string $html_atts
+         * @param array $atts
+         * @param string $content
+         * @param string $tag
+         */
+        $html = apply_filters( 'tailor_shortcode_html', $html, $outer_html, $inner_html, $html_atts, $atts, $content, $tag );
+
+        return $html;
     }
 
     add_shortcode( 'tailor_flipcard', 'tailor_shortcode_flipcard_element' );
